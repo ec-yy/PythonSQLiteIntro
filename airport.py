51ca51e4598adb101ae321783dc4361a38e372d7
Edward Choi <ec2603@bath.ac.uk>
2026-06-06 06:52:13 +0000

    Jun 6, #2

--- airport.py
+++ airport.py
@@ -1,19 +1,22 @@
 from rule import non_empty_input, boolean_input, conformed_id_input, record_exists
 
-
+# Function to add a new airport
 def add_new_airport(connection, cursor):
     print("\n<--- Add a New Airport --->")
 
+    #Provide an airport ID that conforms to a prescribed format (i.e., 3 uppercase letters)
     airport_id = conformed_id_input("Please provide airport ID (e.g. NRT): ", r"^[A-Z]{3}$", "e.g., NRT")
 
     if record_exists(cursor, "Airport", "airport_id", airport_id):
         print(f"Sorry. Airport {airport_id} exists in the table. Go to option 7 of main menu if you would like to update airport records.")
         return
 
+    # Provide an airport name, country and city. All of them must be non-empty strings
     airport_name = non_empty_input("Airport name: ")
     country = non_empty_input("Country: ")
     city = non_empty_input("City: ")
 
+    # Try to add a new airport to the table Airport based on user input and catch any exception
     try:
         cursor.execute(
             """
@@ -28,11 +31,14 @@ def add_new_airport(connection, cursor):
         print("Failure in addition of airport: ", e)
 
 
+#Function to view or update specific airport information
 def view_update_airport(connection, cursor):
     print("\n<--- View or Update Airport --->")
 
+    # Provide an airport ID that conforms to a prescribed format (i.e., 3 uppercase letters)
     airport_id = conformed_id_input("Please provide airport ID (e.g. NRT): ", r"^[A-Z]{3}$", "e.g., NRT")
 
+    # Try to fetch the airport record based on airport ID provided by user.
     cursor.execute(
         """
         SELECT *
@@ -52,6 +58,7 @@ def view_update_airport(connection, cursor):
     print(f"   Country : {row[2]}")
     print(f"   City    : {row[3]}")
 
+    # Prompt user if they want to update the airport information.
     user_input = boolean_input("Do you want to update the information of this Airport record? (Y/N): ")
     if user_input == 'N':
         return
@@ -65,6 +72,7 @@ def view_update_airport(connection, cursor):
     updated_country = revised_country if revised_country else row[2]
     updated_city = revised_city if revised_city else row[3]
 
+    # Try to update the airport record based on user input and catch any exception.
     try:
         cursor.execute(
             """
--- route.py
+++ route.py
@@ -1,21 +1,26 @@
-from rule import non_empty_input, positive_integer_input, boolean_input, conformed_id_input, record_exists
+from rule import non_empty_input, positive_integer_input, conformed_id_input, record_exists
 
-    
+
+#Function to add a new route    
 def add_new_route(connection, cursor):
     print("\n<--- Add New Route --->")
+    
+    # Provide a route ID that conforms to a prescribed format (i.e., 2 uppercase letters followed by 3 digits)
     route_id = conformed_id_input("Please provide Route ID (e.g. CX001): ", r"^[A-Z]{2}[0-9]{3}$", "e.g., CX001")
 
     if record_exists(cursor, "Route", "route_id", route_id):
         print(f"Route {route_id} already exists.")
         return
 
+    # Provide a duration (measured in minutes) that is a positive integer
     duration = positive_integer_input("Please provide duration (in minutes): ")
 
+    # Provide origin and destination airports that both conform to a prescribed format (i.e., 3 uppercase letters) and are different
     origin, destination = None, None
 
     while True:
         if origin is None:
-            origin_input = non_empty_input("Enter Origin Airport ID: ").upper()
+            origin_input = conformed_id_input("Enter Origin Airport ID (e.g., NRF): ", r"^[A-Z]{3}$", "e.g., NRT")
             if not record_exists(cursor, "Airport", "airportId", origin_input):
                 print(f"Origin airport {origin_input} not found. Please try again.")
                 continue
@@ -37,7 +42,7 @@ def add_new_route(connection, cursor):
 
     print(f"Origin and destination are validated. We will use route from {origin} to {destination}.")
 
-
+    # Try to add a new route to the table Route based on user input and catch any exception.
     try:
         cursor.execute("""
             INSERT INTO Route (route_id, duration_minutes, origin_airport_id, destination_airport_id)
